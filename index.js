981ee75816cb1acf5172e1ef59e4c9c188045163
NDevTK <31563761+NDevTK@users.noreply.github.com>
2019-10-15 23:08:07 +0100

    Create index.js

---
+++ index.js
@@ -0,0 +1,50 @@
+// NDev 2019
+const WebSocket = require('ws');
+const PORT = process.env.PORT || 8080;
+const net = require('net');
+const ValidHostnameRegex = /^(([a-zA-Z0-9]|[a-zA-Z0-9][a-zA-Z0-9\-]*[a-zA-Z0-9])\.)*([A-Za-z0-9]|[A-Za-z0-9][A-Za-z0-9\-]*[A-Za-z0-9])$/;
+const wss = new WebSocket.Server({ port: PORT });
+
+function checkInput(ws, host, port) {
+	if(!ValidHostnameRegex.test(host)) ws.close();
+	if (!Number.isInteger(port) || port < 1 || port > 65535) ws.close();
+}
+
+function connect(ws, host, port) {
+	let client = net.connect(port, host);
+	client.setEncoding('utf8');
+	client.on('data', function(data) {
+		ws.send(data);
+	});
+    client.on('timeout', function(e) {
+		ws.send("timeout");
+		ws.close();
+        client.end();
+    });       
+    client.on('close', function(e) {
+		ws.send("closed");
+		ws.close();
+    });
+	client.on('error', function(e) {
+		ws.send("error");
+		ws.close();
+    });
+	return client;
+}
+
+wss.on('connection', function connection(ws) {
+	var client;
+    ws.on('message', function incoming(message) {
+      if(!message) return;
+      if(!addr) {
+        var addr = message.split(":");
+        if(addr.length > 2) return ws.close();
+        var host = addr[0];
+        var port = (addr.length === 2) ? port = addr[1] : 23;
+		checkInput(ws, host, port);
+		client = connect(ws, host, port);
+    } else {
+		client.write(chunk);
+    }
+	});
+});
